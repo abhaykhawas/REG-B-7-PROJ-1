266828c2c5354143ae4e3c3dcd2e15d1254837b5
Abhay <abhayk@regenesys.com>
2025-06-11 23:07:24 +0530

    Finished the Counter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  // NOTE for useState : 
+  // 1. counter : a variable which at first has a value of 0.
+  // 2. setCounter : a function that can change counter [0, func]
 
   return (
     <div style={{height:'100vh', background:'black', color: 'white'}} className='container'>
       <h1>Counter</h1>
       <div className="counter-section">
-        <button>+</button>
-        <h3>0</h3>
-        <button>-</button>
+        <button onClick={() => setCounter(counter+1)}>+</button>
+        <h3>{counter}</h3>
+        <button onClick={() => setCounter(counter-1)}>-</button>
       </div>
     </div>
   )
